3913ea5da9aafdfdfd1fd2b7096e7cec1339f488
Ильгиз <ilgizagliullin@gmail.com>
2024-10-29 22:50:09 +0500

    1.18 Django Rest Fr,  CRUD, comments

--- drf_app/object_detection/urls.py
+++ drf_app/object_detection/urls.py
@@ -3,12 +3,14 @@
 from django.urls import path, include  # Импортируем функции path и include для определения маршрутов
 from .views import home, index  # Импортируем представления home и index из текущего приложения
 from rest_framework.routers import DefaultRouter  # Импортируем DefaultRouter для создания маршрутов API
-from .views import ItemViewSet  # Импортируем ItemViewSet для работы с элементами
+from .views import ItemViewSet, ItemListView, ItemDetailView  # Импортируем ItemViewSet для работы с элементами
 
 router = DefaultRouter()  # Создаем экземпляр маршрутизатора
 router.register(r'items', ItemViewSet)  # Регистрируем ItemViewSet под URL 'items'
 
 urlpatterns = [
+    path('items/', ItemListView.as_view(), name='item-list'),
+    path('items/<int:pk>/', ItemDetailView.as_view(), name='item-detail'),
     path('', index, name='index'),  # Определяем маршрут для главной страницы приложения
     path('home/', home, name='home'),  # Определяем маршрут для страницы "Home"
     path('api/', include(router.urls)),  # Включаем маршруты API, определенные в router
--- drf_app/object_detection/views.py
+++ drf_app/object_detection/views.py
@@ -1,11 +1,11 @@
 # C:\Users\Ilgiz Agliullin\PycharmProjects\Graduate_work\drf_app\object_detection\views.py
 
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Item  # Импортируйте вашу модель предмета
 from .serializers import ItemSerializer  # Импортируйте сериализатор предмета
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include  # Импортируйте path и include
 
@@ -18,6 +18,37 @@ def home(request):
 def index(request):
     return render(request, 'object_detection/home.html')  # Здесь указываем, что отображается home.html
 
+class ItemListView(APIView):
+    def get(self, request):
+        items = Item.objects.all()
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ItemDetailView(APIView):
+    def get(self, request, pk):
+        item = Item.objects.get(pk=pk)
+        serializer = ItemSerializer(item)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        item = Item.objects.get(pk=pk)
+        serializer = ItemSerializer(item, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        item = Item.objects.get(pk=pk)
+        item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()  # Запрос всех предметов
